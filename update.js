10c3632725cbbdc5fc1d0c9f476dfced6965f3f7
Nike Shian G. Guray <nikkiguray12@gmail.com>
2022-10-21 10:33:04 +0800

    addition of update.js

---
+++ update.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const update = function(note, oldNote){
+    const newNote = JSON.parse(oldNote).map(function(n, id){
+        if(n.id == note.id){
+            n.title = note.title;
+            n.body = note.body;
+        }
+        return n
+    })
+    fs.writeFileSync('note.txt', JSON.stringify(newNote))
+}
+module.exports = update
